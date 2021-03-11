eeaba727e52c04779e8fc88f20d7f54eb7af7dc3
Florian Weiss <floweiss6@gmail.com>
2021-03-11 14:17:33 +0100

    split SearchView from HomeView + add factory

---
+++ SWE2-TourPlanner/Factory/TourListViewModelFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using SWE2_TourPlanner.ViewModels;
+
+namespace SWE2_TourPlanner.Factory
+{
+    public class TourListViewModelFactory : IViewModelFactory
+    {
+        public object CreateViewModel(DependencyObject sender)
+        {
+            TourListViewModel vm = new TourListViewModel();
+            return vm;
+        }
+    }
+}
---
+++ SWE2-TourPlanner/ViewModels/TourListViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SWE2_TourPlanner.ViewModels
+{
+    public class TourListViewModel : BaseViewModel
+    {
+        public ICommand AddTourCommand => new RelayCommand(AddTour);
+
+        private void AddTour(object sender)
+        {
+            Debug.WriteLine("Add Tour clicked");
+        }
+    }
+}
